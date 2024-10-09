5d749532f6c923555a6c5bbc70e873baf369afb4
emreertugrl <emrertugrl@gmail.com>
2024-10-09 16:06:03 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Detail from "./pages/detail/index";
+import Home from "./pages/home/index";
+import Header from "./components/header";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="*" element={"<NotFound />"} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
