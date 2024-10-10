b91ae5a467ebb970857677107454ea5f5ff4373c
emreertugrl <emrertugrl@gmail.com>
2024-10-10 11:33:11 +0300

    map rendered to screen

--- src/pages/home/Map.jsx
+++ src/pages/home/Map.jsx
@@ -1,5 +1,47 @@
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  ZoomableGroup,
+} from "react-simple-maps";
+import Container from "./../../components/container/index";
+
 const Map = () => {
-  return <div>Map</div>;
+  const geoUrl = "https://ismailarilik.com/react-covid-maps/geo.json";
+
+  return (
+    <Container design="max-md:!p-0 my-10 ">
+      <h1 className="p-5 text-2xl font-semibold">Ülke Seçin</h1>
+      <div className="border shadow-lg   md:rounded-xl  bg-gray-200">
+        <ComposableMap>
+          <ZoomableGroup>
+            <Geographies geography={geoUrl}>
+              {({ geographies }) =>
+                geographies.map((geo) => (
+                  <Geography
+                    key={geo.rsmKey}
+                    geography={geo}
+                    style={{
+                      default: {
+                        fill: "white",
+                        stroke: "gray",
+                      },
+                      hover: {
+                        fill: "#DB2777",
+                      },
+                      pressed: {
+                        fill: "orange",
+                      },
+                    }}
+                  />
+                ))
+              }
+            </Geographies>
+          </ZoomableGroup>
+        </ComposableMap>
+      </div>
+    </Container>
+  );
 };
 
 export default Map;
