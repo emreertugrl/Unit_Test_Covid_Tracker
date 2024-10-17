b9f1968743ea1f25563c43d1c0308dc2d9cdeb0b
emreertugrl <emrertugrl@gmail.com>
2024-10-17 17:17:14 +0300

    Statistic components api test completed.

--- src/pages/home/Statistic.jsx
+++ src/pages/home/Statistic.jsx
@@ -8,11 +8,12 @@ import HomeLoader from "../../components/loaders/HomeLoader";
 const Statistic = () => {
   const [isLoading, setLoading] = useState(true);
   const [totals, setTotals] = useState(null);
+  const [isError, setIsError] = useState(false);
   useEffect(() => {
     api
       .get("/totals")
       .then((res) => setTotals(res.data[0]))
-      .catch((err) => console.log(err))
+      .catch(() => setIsError(true))
       .finally(() => setLoading(false));
   }, []);
 
@@ -21,6 +22,10 @@ const Statistic = () => {
       <div className="bg-white shadow-lg rounded-xl p-5 grid grid-cols-3 gap-5 mt-[-42px] md:mt-[-60px]">
         {isLoading ? (
           <HomeLoader />
+        ) : isError ? (
+          <p className="grid  col-span-3 text-center ">
+            İstatistikler alınamıyor...
+          </p>
         ) : (
           <>
             <Item
