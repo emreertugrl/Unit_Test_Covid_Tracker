42640aa8089bc9ac63292ff1c063425eeb006d26
emreertugrl <emrertugrl@gmail.com>
2024-11-08 15:01:25 +0300

    open readme file

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,5 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+// REACT_APP_API_KEY =eed126f9bcmshc5525da323f2af8p1ab61fjsn34753381407b
+import "@testing-library/jest-dom";
